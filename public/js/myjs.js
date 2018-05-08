e3a9c8ac482a024cbd6c579caa22f8418e123a18
O Max Anderson <anderso2@miamioh.edu>
2018-05-08 14:23:03 -0400

    Added a lot of content to the main section

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
 });
 
 function setContentHeight() {
+  // first move custom-container down for navbar
+  $("#custom-container").css('margin-top', $('.navbar').outerHeight());
+
+  // set the size of the header container to be equal to either the size of the image
+  // or the size of the viewport
   var height = $(window).outerHeight() < $('.jumbotron-img').outerHeight() ?
     $(window).outerHeight() : $('.jumbotron-img').outerHeight();
   $('#header-img-container').css('height', height);
 
+  // position the header a third of the way down the image
   $('#main-header').css('padding-top', $('.jumbotron-img').outerHeight() / 3);
 }
